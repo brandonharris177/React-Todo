449ac973090f59ac5b7b5e65578c28ca60d36422
Brandon-Harris <brandonharris177@gmail.com>
2019-09-01 15:44:36 -0600

    items displayed from ToDoList

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,38 @@
 // your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+// feel free to change this component.js into ToDoData.js
+import React from 'react'
+
+const ToDoData =
+[
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
+class ToDoList extends React.Component {
+
+constructor() {
+    super();
+    this.state = {
+      toDo: ToDoData
+    };
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.toDo.map(todo => <h2>{todo.task}</h2>)}
+      </>
+    );
+  }
+}
+
+
+export default ToDoList;
